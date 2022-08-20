0a414fba33d169f3e4ee9a739e4f75d26b34e2fb
league <league@imac-20.attlocal.net>
2022-08-20 12:53:09 -0700

    e

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,79 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
-		
+	Robot rob = new Robot();
 	public void run() {
+		
 		// Check the recipe to find out what code to put here
+		rob.penDown();
+		rob.setSpeed(9999991);
+		rob.setX(19);
+		rob.setY(481);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(270);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(270);
+		rob.move(30);
+		rob.turn(180);
+
+
+		
+		
+	
+		
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	private void houses() {
+		rob.penDown();
+		rob.setSpeed(9999991);
+		rob.setX(19);
+		rob.setY(481);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(270);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(270);
+		rob.move(30);
+	}
+
+	private void method1(Object turn) {
+		// TODO Auto-generated method stub
+		
 	}
 }
